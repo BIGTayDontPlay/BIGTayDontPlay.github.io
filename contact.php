eaa3cc918756c4675da2af912ad7253a768e69a1
BIGTayDontPlay <51804971+BIGTayDontPlay@users.noreply.github.com>
2024-10-10 22:57:43 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\nPhone: $phone\nMessage: $message";
     $headers = "From: $email\r\n";
-    
+
     if (mail($to, $subject, $body, $headers)) {
         echo "Message successfully sent!";
     } else {
